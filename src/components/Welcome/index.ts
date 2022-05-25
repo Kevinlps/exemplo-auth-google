93a50490cb0151182c954cb96c53a9d094c6f19c
Kevin <kevinlopes3012@gmail.com>
2022-05-24 21:59:22 -0400

    Autenticação pelo google

---
+++ src/components/Welcome/index.ts
@@ -0,0 +1,15 @@
+import './styles.css'
+
+const renderWelcomePanel = (container: HTMLElement) => {
+    const userName = localStorage.getItem('userName')
+
+    const htmlContent =`
+    <div id="Welcome"> 
+        <h1>Olá, ${userName}!</h1>
+        <a href="logout.html">Sair</a>
+    </div>
+ ` 
+    container.innerHTML = htmlContent
+}
+
+export default renderWelcomePanel
---
+++ src/login.ts
@@ -0,0 +1,5 @@
+import renderLoginButton from './components/loginButton'
+import './styles.css'
+
+const app = <HTMLDivElement>document.getElementById('app')
+renderLoginButton(app)
