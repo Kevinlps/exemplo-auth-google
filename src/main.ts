cbdb7bebe0cc324efc4f27e6dc7c82d5bf9fb91d
Kevin <kevinlopes3012@gmail.com>
2022-05-25 18:55:07 -0400

    funcionando

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import renderWelcomePanel from './components/Welcome'
 import './styles.css'
 
-const app = <HTMLDivElement>document.getElementById('#app')
+const app = <HTMLDivElement>document.getElementById('app')
 const token = localStorage.getItem('token')
 
 if(token) {
